48450db8351b7a4fbfe1b10b5dc0c846e357de30
Atul Rajput <eatulrajput.com>
2025-05-30 21:29:22 +0530

    Add Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Blog from "./components/Blog";
 import MySpace from "./components/MySpace";
 import GTagRouteTracker from './components/GTagRouteTracker';
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
-    return (
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        // Simulate loading delay
+        const timer = setTimeout(() => setLoading(false), 2000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return <LoadingSpinner loading={loading} />;
+    }
+
+    return (
         <Router>
             <GTagRouteTracker />
             <Navbar />
